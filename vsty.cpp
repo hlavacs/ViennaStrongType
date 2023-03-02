5f4b9ec9f52fe4d5280a0f611b648bca16510d50
Helmut <Helmut@DESKTOP-QCHMISS>
2023-03-02 13:39:07 +0100

    Introduced PP for phantom type chaining

--- vsty.cpp
+++ vsty.cpp
@@ -2,8 +2,14 @@
 #include "vsty.h"
 
 
-int main() {
+using strong_int_t = vsty::strong_type_t<int, 1>;
+using strong_size_t = vsty::strong_integral_t<int, strong_int_t::PP + 1>;
+using strong_uint32_t = vsty::strong_type_t<int, strong_size_t::PP + 1>;
 
+
+int main() {
+	static_assert(!std::is_same_v<strong_int_t, strong_size_t>, "strong_int_t and strong_size_t are the same type!");
+	static_assert(!std::is_same_v<strong_size_t, strong_uint32_t>, "strong_size_t and strong_uint32_t are the same type!");
 
 }
 
--- vsty.h
+++ vsty.h
@@ -10,12 +10,12 @@ namespace vsty {
 	*
 	* T...the type
 	* P...phantom type as unique ID
-	* D...default value (=null value)
-	* U...number of upper bits (if integer is cut into 2 values)
 	*/
-    template<typename T, size_t P = 0>
+    template<typename T, auto P = 0>
     struct strong_type_t {
         T value{};
+		static const auto PP = P;
+
         strong_type_t() = default;
         explicit strong_type_t(const T& v) noexcept { value = v; };
         explicit strong_type_t(T&& v) noexcept { value = std::move(v); };
@@ -37,220 +37,141 @@ namespace vsty {
     };
 
 
-	template<typename T, auto D, size_t P = 0>
-	struct strong_null_t {
+	/**
+	* \brief Strong type with a null value
+	*
+	* T...the type
+	* D...default value (=null value)
+	* P...phantom type as unique ID
+	*/
+	template<typename T, auto D, auto P = 0>
+	struct strong_type_null_t {
 		T value{D};
-		strong_null_t() = default;
-		explicit strong_null_t(const T& v) noexcept { value = v; };
-		explicit strong_null_t(T&& v) noexcept { value = std::move(v); };
+		static const auto PP = P;
 
-		strong_null_t(const strong_null_t<T, D, P>& v) noexcept { value = v.value; };
-		strong_null_t(strong_null_t<T, D, P>&& v) noexcept { value = std::move(v.value); };
+		strong_type_null_t() = default;
+		explicit strong_type_null_t(const T& v) noexcept { value = v; };
+		explicit strong_type_null_t(T&& v) noexcept { value = std::move(v); };
+
+		strong_type_null_t(const strong_type_null_t<T, D, P>& v) noexcept { value = v.value; };
+		strong_type_null_t(strong_type_null_t<T, D, P>&& v) noexcept { value = std::move(v.value); };
 
 		operator const T& () const noexcept { return value; }
 		operator T& () noexcept { return value; }
 
-		strong_null_t<T, D, P>& operator=(const T& v) noexcept { value = v; return *this; };
-		strong_null_t<T, D, P>& operator=(T&& v) noexcept { value = std::move(v); return *this; };
-		strong_null_t<T, D, P>& operator=(const strong_null_t<T, D, P>& v) noexcept { value = v.value; return *this; };
-		strong_null_t<T, D, P>& operator=(strong_null_t<T, D, P>&& v) noexcept { value = std::move(v.value); return *this; };
+		strong_type_null_t<T, D, P>& operator=(const T& v) noexcept { value = v; return *this; };
+		strong_type_null_t<T, D, P>& operator=(T&& v) noexcept { value = std::move(v); return *this; };
+		strong_type_null_t<T, D, P>& operator=(const strong_type_null_t<T, D, P>& v) noexcept { value = v.value; return *this; };
+		strong_type_null_t<T, D, P>& operator=(strong_type_null_t<T, D, P>&& v) noexcept { value = std::move(v.value); return *this; };
 
-		auto operator<=>(const strong_null_t<T, D, P>& v) noexcept { return value <=> v.value; };
+		auto operator<=>(const strong_type_null_t<T, D, P>& v) noexcept { return value <=> v.value; };
 
 		bool has_value() const noexcept { return value != D;}
 
 		struct hash {
-			std::size_t operator()(const strong_null_t<T, D, P>& tag) const noexcept { return std::hash<T>()(tag.value); };
+			std::size_t operator()(const strong_type_null_t<T, D, P>& tag) const noexcept { return std::hash<T>()(tag.value); };
 		};
 	};
 
 
 	/**
-	* \brief Strong type for integers.
+	* \brief Strong integral type, like size_t or uint32_t. Can be split into two integral values.
 	*
 	* T...the integer type
-	* P...phantom type as unique ID
 	* D...default value (=null value)
-	* U...number of upper bits (if integer is cut into 2 values)
+	* P...phantom type as unique ID
+	* U...number of upper bits (if integer is cut into 2 values), or else 0
 	*/
-	template<typename T, auto D = std::numeric_limits<T>::max(), size_t P = 0, size_t U = 0>
-	struct strong_int_t {
-		static const T L = sizeof(T) * 8 - U; //number of lower bits (if integer is cut into 2 values)
+	template<typename T, auto D = std::numeric_limits<T>::max(), auto P = 0, size_t U = 0>
+	struct strong_integral_t {
+		static const size_t L = sizeof(T) * 8ul - U; //number of lower bits (if integer is cut into 2 values)
 
 		T value{ D };
+		static const auto PP = P;
 
-		strong_int_t() = default;
+		strong_integral_t() = default;
 
-		/**
-		* \brief Constructor.
-		* \param[in] u A POD type that is used for setting the value.
-		*/
 		template<typename U>
 			requires (std::is_convertible_v<std::decay_t<U>, T>&& std::is_pod_v<std::decay_t<U>>)
-		explicit strong_int_t(U&& u) noexcept : value{ static_cast<T>(u) } {};
+		explicit strong_integral_t(U&& u) noexcept : value{ static_cast<T>(u) } {};
 
-		/**
-		* \brief Copy assignment.
-		* \param[in] rhs Any POD int type.
-		*/
 		template<typename U>
 			requires (std::is_convertible_v<std::decay_t<U>, T>&& std::is_pod_v<std::decay_t<U>>)
 		void operator=(U&& rhs) noexcept { value = static_cast<T>(rhs); };
 
-		/**
-		* \brief Yield the int value.
-		* \returns the int value.
-		*/
 		operator const T& () const { return value; }
 
-		/**
-		* \brief Yield the int value.
-		* \returns the int value.
-		*/
 		operator T& () { return value; }
 
-		/**
-		* \brief Comparison operator.
-		* \returns the default comparison.
-		*/
-		auto operator<=>(const strong_int_t<T, D, P, U>& v) noexcept { return value <=> v.value; };
+		auto operator<=>(const strong_integral_t<T, D, P, U>& v) noexcept { return value <=> v.value; };
 
-		/**
-		* \brief Comparison operator.
-		* \returns the comparison between this value and another int.
-		*/
 		template<typename U>
 			requires std::is_convertible_v<U, T>
 		auto operator<(const U& v) noexcept { return value < static_cast<T>(v); };
 
-		/**
-		* \brief Left shift operator.
-		* \param[in] L Number of bits to left shift.
-		* \returns the value left shifted.
-		*/
 		T operator<<(const size_t L) noexcept { return value << L; };
-
-		/**
-		* \brief Right shift operator.
-		* \param[in] L Number of bits to right shift.
-		* \returns the value left shifted.
-		*/
 		T operator>>(const size_t L) noexcept { return value >> L; };
-
-		/**
-		* \brief And operator.
-		* \param[in] L Number that should be anded bitwise.
-		* \returns the bew value that was anded to the old value.
-		*/
 		T operator&(const size_t L) noexcept { return value & L; };
 
-		/**
-		* \brief Pre-increment operator.
-		* \returns the value increased by 1.
-		*/
-		strong_int_t<T, D, P, U> operator++() noexcept {
+		strong_integral_t<T, D, P, U> operator++() noexcept {
 			value++;
 			if (!has_value()) value = 0;
 			return *this;
 		};
 
-		/**
-		* \brief Post-increment operator.
-		* \returns the old value before increasing by 1.
-		*/
-		strong_int_t<T, D, P, U> operator++(int) noexcept {
-			strong_int_t<T, D, P, U> res = *this;
+		strong_integral_t<T, D, P, U> operator++(int) noexcept {
+			strong_integral_t<T, D, P, U> res = *this;
 			value++;
 			if (!has_value()) value = 0;
 			return res;
 		};
 
-		/**
-		* \brief Pre-decrement operator.
-		* \returns the value decreased by 1.
-		*/
-		strong_int_t<T, D, P, U> operator--() noexcept {
+		strong_integral_t<T, D, P, U> operator--() noexcept {
 			--value;
 			if (!has_value()) --value;
 			return *this;
 		};
 
-		/**
-		* \brief Post-decrement operator.
-		* \returns the value before decreasing by 1.
-		*/
-		strong_int_t<T, D, P, U> operator--(int) noexcept {
-			strong_int_t<T, D, P, U> res = *this;
+		strong_integral_t<T, D, P, U> operator--(int) noexcept {
+			strong_integral_t<T, D, P, U> res = *this;
 			value--;
 			if (!has_value()) value--;
 			return res;
 		};
 
-		/**
-		* \brief Create a hash value.
-		*/
 		struct hash {
-			/**
-			* \param[in] tg The input int value.
-			* \returns the hash of the int value.
-			*/
-			std::size_t operator()(const strong_int_t<T, D, P, U>& tg) const { return std::hash<T>()(tg.value); };
+			std::size_t operator()(const strong_integral_t<T, D, P, U>& tg) const { return std::hash<T>()(tg.value); };
 		};
 
-		/**
-		* \brief Equality comparison as function.
-		* \returns true if the value is not null (the default value).
-		*/
 		struct equal_to {
 			constexpr bool operator()(const T& lhs, const T& rhs) const { return lhs == rhs; };
 		};
 
-		/**
-		* \brief Determine whether the value is not null.
-		* \returns true if the value is not null (the default value).
-		*/
 		bool has_value() const {
 			return value != D;
 		}
 
-		/**
-		* \brief Set the upper value (if split into two integers).
-		* \param[in] v New upper value.
-		*/
 		void set_upper(T v) {
 			const T LMASK = (1 << L) - 1;
 			value = (value & LMASK) | (value << L);
 		}
 
-		/**
-		* \brief Return the upper value (if split into two integers).
-		* \returns the upper value.
-		*/
 		T get_upper() {
 			return (value >> L);
 		}
 
-		/**
-		* \brief Set the lower value (if split into two integers).
-		* \param[in] v New lower value.
-		*/
 		void set_lower(T v) {
 			const T LMASK = (1 << L) - 1;
 			const T UMASK = ((1 << U) - 1) << L;
 			value = (value & UMASK) | (v & LMASK);
 		}
 
-		/**
-		* \brief Return the lower value (if split into two integers).
-		* \returns the lower value.
-		*/
 		T get_lower() {
 			const T LMASK = (1 << L) - 1;
 			return (value & LMASK);
 		}
 
 	};
-
 
 }
 
