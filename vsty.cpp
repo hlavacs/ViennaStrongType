ccf9f1196f3c519e91e2b258a575f550c6bb6f66
Helmut <Helmut@DESKTOP-QCHMISS>
2023-03-02 14:41:31 +0100

    added a type counter

--- vsty.cpp
+++ vsty.cpp
@@ -2,9 +2,9 @@
 #include "vsty.h"
 #include <string>
 
-using strong_int_t = vsty::strong_type_t<int, 1111 >;
-using strong_size_t = vsty::strong_integral_t<int, strong_int_t::PP + 1 >;
-using strong_uint32_t = vsty::strong_type_t<int, strong_size_t::PP + 1 >;
+using strong_int_t = vsty::strong_type_t<int, vsty::counter<> >;
+using strong_size_t = vsty::strong_integral_t<int, vsty::counter<> >;
+using strong_uint32_t = vsty::strong_type_t<int, vsty::counter<> >;
 
 
 int main() {
--- vsty.h
+++ vsty.h
@@ -174,6 +174,48 @@ namespace vsty {
 	};
 
 
+	//--------------------------------------------------------
+	//type counter lifted from https://mc-deltat.github.io/articles/stateful-metaprogramming-cpp20
+
+	template<unsigned N>
+	struct reader {
+		friend auto counted_flag(reader<N>);
+	};
+
+
+	template<unsigned N>
+	struct setter {
+		friend auto counted_flag(reader<N>) {}
+
+		static constexpr unsigned n = N;
+	};
+
+
+	template<
+		auto Tag,
+		unsigned NextVal = 0
+	>
+	[[nodiscard]]
+	consteval auto counter_impl() {
+		constexpr bool counted_past_value = requires(reader<NextVal> r) {
+			counted_flag(r);
+		};
+
+		if constexpr (counted_past_value) {
+			return counter_impl<Tag, NextVal + 1>();
+		}
+		else {
+			setter<NextVal> s;
+			return s.n;
+		}
+	}
+
+
+	template<
+		auto Tag = [] {},
+		auto Val = counter_impl<Tag>()
+	>
+	constexpr auto counter = Val;
 
 }
 
