735d6251104867fa08b8c8f6fab2029203fc7e9a
Helmut <Helmut@DESKTOP-QCHMISS>
2023-03-03 15:18:31 +0100

    redesigned the classes

--- vsty.cpp
+++ vsty.cpp
@@ -3,21 +3,30 @@
 #include <string>
 
 using strong_int_t		= vsty::strong_type_t< size_t, vsty::counter<> >;
-using strong_uint32_t	= vsty::strong_type_t< size_t, vsty::counter<> >;
+using strong_uint32_t	= vsty::strong_type_t< uint32_t, vsty::counter<> >;
 
 using strong_size_t		= vsty::strong_integral_t< size_t, vsty::counter<> >;
 using strong_size_t2	= vsty::strong_integral_t< size_t, vsty::counter<> >;
 
+static_assert(!std::is_same_v<strong_int_t, strong_size_t>, "strong_int_t and strong_size_t are the same type!");
+static_assert(!std::is_same_v<strong_size_t, strong_uint32_t>, "strong_size_t and strong_uint32_t are the same type!");
+static_assert(!std::is_same_v<strong_size_t, strong_size_t2>, "strong_size_t and strong_size_t2 are the same type!");
+static_assert(!std::is_same_v<strong_int_t, strong_uint32_t>, "strong_int_t and strong_uint32_t are the same type!");
 
+void f(strong_int_t a) { return; }
+void g(strong_size_t a) { return; }
+void h(strong_size_t2 a) { return; }
 
 int main() {
-	static_assert(!std::is_same_v<strong_int_t, strong_size_t>, "strong_int_t and strong_size_t are the same type!");
 
-	static_assert(!std::is_same_v<strong_size_t, strong_uint32_t>, "strong_size_t and strong_uint32_t are the same type!");
+	//f(2); //error
+	//g(2); //error
+	//h(3); //error
 
-	static_assert(!std::is_same_v<strong_size_t, strong_size_t2>, "strong_size_t and strong_size_t2 are the same type!");
-
-	static_assert(!std::is_same_v<strong_int_t, strong_uint32_t>, "strong_int_t and strong_uint32_t are the same type!");
+	f(strong_int_t{ 2 });
+	g(strong_size_t{ 2 });
+	h(strong_size_t2{ 3 });
+	
 
 }
 
--- vsty.h
+++ vsty.h
@@ -5,6 +5,121 @@
 
 namespace vsty {
 
+
+	/**
+	* \brief General strong type
+	*
+	* T...the type
+	* P...phantom type as unique ID (can use __COUNTER__ or vsty::counter<>)
+	*/
+    template<typename T, auto P >
+    struct strong_type_t {
+        T value{};
+
+        strong_type_t() = default;
+        explicit strong_type_t(const T& v) noexcept { value = v; };
+        explicit strong_type_t(T&& v) noexcept { value = std::move(v); };
+
+        strong_type_t(const strong_type_t<T, P>& v) noexcept { value = v.value; };
+        strong_type_t(strong_type_t<T, P>&& v) noexcept { value = std::move(v.value); };
+
+        operator const T& () const noexcept { return value; }
+        operator T& () noexcept { return value; }
+
+        strong_type_t<T, P>& operator=(const T& v) noexcept { value = v; return *this; };
+        strong_type_t<T, P>& operator=(T&& v) noexcept { value = std::move(v); return *this; };
+        strong_type_t<T, P>& operator=(const strong_type_t<T, P>& v) noexcept { value = v.value; return *this; };
+        strong_type_t<T, P>& operator=(strong_type_t<T, P>&& v) noexcept { value = std::move(v.value); return *this; };
+
+		auto operator<=>(const strong_type_t<T, P>& v) noexcept { return value <=> v.value; };
+
+		struct equal_to {
+			constexpr bool operator()(const T& lhs, const T& rhs) const { return lhs == rhs; };
+		};
+
+        struct hash {
+            std::size_t operator()(const strong_type_t<T, P>& tag) const noexcept { return std::hash<T>()(tag.value); };
+        };
+    };
+
+
+	/**
+	* \brief Strong type with a null value
+	*
+	* T...the type
+	* D...default value (=null value)
+	* P...phantom type as unique ID (can use __COUNTER__ or vsty::counter<>)
+	*/	
+	template<typename T, auto P, auto D>
+	struct strong_type_null_t : strong_type_t<T, P> {
+		using strong_type_t<T,P>::value;
+		static const T null{D};
+		strong_type_null_t() { value = D; };
+		explicit strong_type_null_t(const T& v) : strong_type_t<T,P>(v) {};
+		bool has_value() const noexcept { return value != D; }
+	};
+
+
+	/**
+	* \brief Strong integral type, like size_t or uint32_t. Can be split into two integral values (upper and lower)
+	*
+	* T...the integer type
+	* P...phantom type as unique ID (can use __COUNTER__ or vsty::counter<>)
+	* U...number of upper bits (if integer is cut into 2 values), or else 0
+	*/	
+	template<typename T, auto P, size_t U = 0>
+		requires std::is_integral_v<std::decay_t<T>>
+	struct strong_integral_t : strong_type_t<T, P>  {
+
+		static const size_t L = sizeof(T) * 8ull - U; //number of lower bits (if integer is cut into 2 values)
+
+		static consteval T lmask() {
+			if (L == 0) return 0ull;
+			if (U == 0) return ~0ull >> (64 - (L + U));
+			return ~0ull >> (64 - L);
+		}
+
+		static const T LMASK = lmask();
+		static const T UMASK = ~LMASK;
+
+		using strong_type_t<T, P>::value;
+		strong_integral_t() noexcept = default;
+		explicit strong_integral_t(const T& v) noexcept : strong_type_t<T, P>(v) {};
+
+		T operator<<(const size_t N) noexcept { return value << N; };
+		T operator>>(const size_t N) noexcept { return value >> N; };
+		T operator&(const size_t N) noexcept { return value & N; };
+
+		auto operator++() noexcept { ++value; return *this; };
+		auto operator++(int) noexcept { return strong_integral_t<T, P, U>(value++); };
+		auto operator--() noexcept { --value; return *this; };
+		auto operator--(int) noexcept { return strong_integral_t<T, P, U>(value--); };
+
+		auto set_upper(T v) noexcept requires std::is_unsigned_v<std::decay_t<T>> { value = (value & LMASK) | (value << L); }
+		auto get_upper()    noexcept requires std::is_unsigned_v<std::decay_t<T>> { return (value >> L); }
+		auto set_lower(T v) noexcept requires std::is_unsigned_v<std::decay_t<T>> { value = (value & UMASK) | (v & LMASK); }
+		auto get_lower()    noexcept requires std::is_unsigned_v<std::decay_t<T>> { return (value & LMASK); }
+	};
+
+
+	/**
+	* \brief Strong integral type with a null value. 
+	*
+	* T...the type
+	* D...default value (=null value)
+	* P...phantom type as unique ID (can use __COUNTER__ or vsty::counter<>)
+	* U...number of upper bits (if integer is cut into 2 values), or else 0
+	*/
+	template<typename T, auto P, auto D = std::numeric_limits<T>::max(), size_t U = 0>
+	struct strong_integral_null_t : strong_type_t<T, P> {
+		using strong_integral_t<T, P>::value;
+		static const T null{ D };
+		strong_integral_null_t() { value = D; };
+		explicit strong_integral_null_t(const T& v) : strong_integral_t<T, P>(v) {};
+		bool has_value() const noexcept { return value != D; }
+	};
+
+
 	//--------------------------------------------------------------------------------------------
 	//type counter lifted from https://mc-deltat.github.io/articles/stateful-metaprogramming-cpp20
 
@@ -32,176 +147,6 @@ namespace vsty {
 
 	template< auto Tag = [] {}, auto Val = counter_impl<Tag>() >
 	constexpr auto counter = Val;
-
-	//--------------------------------------------------------------------------------------------
-
-
-	/**
-	* \brief General strong type
-	*
-	* T...the type
-	* P...phantom type as unique ID
-	*/
-    template<typename T, auto P >
-    struct strong_type_t {
-        T value{};
-
-        strong_type_t() = default;
-        explicit strong_type_t(const T& v) noexcept { value = v; };
-        explicit strong_type_t(T&& v) noexcept { value = std::move(v); };
-
-        strong_type_t(const strong_type_t<T, P>& v) noexcept { value = v.value; };
-        strong_type_t(strong_type_t<T, P>&& v) noexcept { value = std::move(v.value); };
-
-        operator const T& () const noexcept { return value; }
-        operator T& () noexcept { return value; }
-
-        strong_type_t<T, P>& operator=(const T& v) noexcept { value = v; return *this; };
-        strong_type_t<T, P>& operator=(T&& v) noexcept { value = std::move(v); return *this; };
-        strong_type_t<T, P>& operator=(const strong_type_t<T, P>& v) noexcept { value = v.value; return *this; };
-        strong_type_t<T, P>& operator=(strong_type_t<T, P>&& v) noexcept { value = std::move(v.value); return *this; };
-
-        struct hash {
-            std::size_t operator()(const strong_type_t<T, P>& tag) const noexcept { return std::hash<T>()(tag.value); };
-        };
-    };
-
-
-	/**
-	* \brief Strong type with a null value
-	*
-	* T...the type
-	* D...default value (=null value)
-	* P...phantom type as unique ID
-	*/
-	template<typename T, auto P, auto D>
-	struct strong_type_null_t {
-		T value{D};
-
-		strong_type_null_t() = default;
-		explicit strong_type_null_t(const T& v) noexcept { value = v; };
-		explicit strong_type_null_t(T&& v) noexcept { value = std::move(v); };
-
-		strong_type_null_t(const strong_type_null_t<T, P, D>& v) noexcept { value = v.value; };
-		strong_type_null_t(strong_type_null_t<T, P, D>&& v) noexcept { value = std::move(v.value); };
-
-		operator const T& () const noexcept { return value; }
-		operator T& () noexcept { return value; }
-
-		strong_type_null_t<T, P, D>& operator=(const T& v) noexcept { value = v; return *this; };
-		strong_type_null_t<T, P, D>& operator=(T&& v) noexcept { value = std::move(v); return *this; };
-		strong_type_null_t<T, P, D>& operator=(const strong_type_null_t<T, P, D>& v) noexcept { value = v.value; return *this; };
-		strong_type_null_t<T, P, D>& operator=(strong_type_null_t<T, P, D>&& v) noexcept { value = std::move(v.value); return *this; };
-
-		auto operator<=>(const strong_type_null_t<T, P, D>& v) noexcept { return value <=> v.value; };
-
-		bool has_value() const noexcept { return value != D;}
-
-		struct hash {
-			std::size_t operator()(const strong_type_null_t<T, P, D>& tag) const noexcept { return std::hash<T>()(tag.value); };
-		};
-	};
-
-
-	/**
-	* \brief Strong integral type, like size_t or uint32_t. Can be split into two integral values.
-	*
-	* T...the integer type
-	* D...default value (=null value)
-	* P...phantom type as unique ID
-	* U...number of upper bits (if integer is cut into 2 values), or else 0
-	*/
-	template<typename T, auto P, auto D = std::numeric_limits<T>::max(), size_t U = 0>
-	struct strong_integral_t {
-		static const size_t L = sizeof(T) * 8ul - U; //number of lower bits (if integer is cut into 2 values)
-
-		T value{ D };
-
-		strong_integral_t() = default;
-
-		template<typename U>
-			requires (std::is_convertible_v<std::decay_t<U>, T>&& std::is_pod_v<std::decay_t<U>>)
-		explicit strong_integral_t(U&& u) noexcept : value{ static_cast<T>(u) } {};
-
-		template<typename U>
-			requires (std::is_convertible_v<std::decay_t<U>, T>&& std::is_pod_v<std::decay_t<U>>)
-		void operator=(U&& rhs) noexcept { value = static_cast<T>(rhs); };
-
-		operator const T& () const { return value; }
-
-		operator T& () { return value; }
-
-		auto operator<=>(const strong_integral_t<T, P, D, U>& v) noexcept { return value <=> v.value; };
-
-		template<typename U>
-			requires std::is_convertible_v<U, T>
-		auto operator<(const U& v) noexcept { return value < static_cast<T>(v); };
-
-		T operator<<(const size_t L) noexcept { return value << L; };
-		T operator>>(const size_t L) noexcept { return value >> L; };
-		T operator&(const size_t L) noexcept { return value & L; };
-
-		strong_integral_t<T, P, D, U> operator++() noexcept {
-			value++;
-			if (!has_value()) value = 0;
-			return *this;
-		};
-
-		strong_integral_t<T, P, D, U> operator++(int) noexcept {
-			strong_integral_t<T, P, D, U> res = *this;
-			value++;
-			if (!has_value()) value = 0;
-			return res;
-		};
-
-		strong_integral_t<T, P, D, U> operator--() noexcept {
-			--value;
-			if (!has_value()) --value;
-			return *this;
-		};
-
-		strong_integral_t<T, P, D, U> operator--(int) noexcept {
-			strong_integral_t<T, P, D, U> res = *this;
-			value--;
-			if (!has_value()) value--;
-			return res;
-		};
-
-		struct hash {
-			std::size_t operator()(const strong_integral_t<T, P, D, U>& tg) const { return std::hash<T>()(tg.value); };
-		};
-
-		struct equal_to {
-			constexpr bool operator()(const T& lhs, const T& rhs) const { return lhs == rhs; };
-		};
-
-		bool has_value() const {
-			return value != D;
-		}
-
-		void set_upper(T v) {
-			const T LMASK = (1 << L) - 1;
-			value = (value & LMASK) | (value << L);
-		}
-
-		T get_upper() {
-			return (value >> L);
-		}
-
-		void set_lower(T v) {
-			const T LMASK = (1 << L) - 1;
-			const T UMASK = ((1 << U) - 1) << L;
-			value = (value & UMASK) | (v & LMASK);
-		}
-
-		T get_lower() {
-			const T LMASK = (1 << L) - 1;
-			return (value & LMASK);
-		}
-
-	};
-
-
 
 }
 
